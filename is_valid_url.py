bcecd17dd28e2bfef1925cf2f5c5f5b798a33d48
Manideep Malyala <42274353+manideep-malyala@users.noreply.github.com>
2024-12-17 01:26:26 +0530

    update is_valid_url.py

--- is_valid_url.py
+++ is_valid_url.py
@@ -2,5 +2,4 @@ from urllib.parse import urlparse
 
 def is_valid_url(url: str) -> bool:
     r = urlparse(url)
-    print("exe-url-check")
     return r.scheme in {"http", "https"} and bool(r.netloc)
